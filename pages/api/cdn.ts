eb9b160ec1cdc71d8088d7e517550d0b15d12d36
Maximous Black <maximousblk@gmail.com>
2021-07-29 12:09:56 +0000

    no default error page
    api routes don't get error pages it seems

--- pages/api/cdn.ts
+++ pages/api/cdn.ts
@@ -1,29 +1,29 @@
-import { NextApiHandler, NextApiResponse } from 'next';
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'node-fetch';
 import { supabase } from '@/lib/supabase';
 
-const handler: NextApiHandler = async (req, res) => {
+const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const slug = req.query.s as string;
   const match = /^([^\/@]+)\/([^\/@]+)@([^\/]+)\/(.*)$/.exec(slug);
 
   if (!match) {
     res.status(404);
-    res.end();
+    res.end('Not Found');
   } else {
     const [, authorName, moduleName, versionName, filepath] = match;
 
     if (!authorName) {
       res.status(404);
-      res.end();
+      res.end('Not Found');
     } else if (!moduleName) {
       res.status(404);
-      res.end();
+      res.end('Not Found');
     } else if (!versionName) {
       res.status(404);
-      res.end();
+      res.end('Not Found');
     } else if (!filepath) {
       res.status(404);
-      res.end();
+      res.end('Not Found');
     } else {
       let { data: Files, error } = await supabase
         .from('File')
@@ -35,15 +35,15 @@ const handler: NextApiHandler = async (req, res) => {
 
       if (error) {
         res.status(500);
-        res.end();
+        res.end('Internal Server Error');
         throw new Error(`${error.message} (hint: ${error.hint})`);
       } else if (Files.length < 1) {
         res.status(404);
-        res.end();
+        res.end('Not Found');
       } else if (Files.length > 1) {
         // more than one files with same primary key @@id([author, module, version, path])
         res.status(500);
-        res.end();
+        res.end('Internal Server Error');
         throw new Error(
           `Found ${Files.length} "Files" with primary key @@id([${authorName}, ${moduleName}, ${versionName}, ${filepath}])`
         );
@@ -60,7 +60,6 @@ const handler: NextApiHandler = async (req, res) => {
           'stale-while-revalidate=31536000',
           'immutable',
         ]);
-
         res.status(data.status);
         data.body.pipe(res);
       }
