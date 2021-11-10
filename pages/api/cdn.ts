c92b4c02c7602173a785fe987503c12509086f97
Maximous Black <maximousblk@gmail.com>
2021-11-10 04:24:46 +0000

    upgrade to new db

--- pages/api/cdn.ts
+++ pages/api/cdn.ts
@@ -2,15 +2,17 @@ import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'node-fetch';
 import { supabase } from '@/lib/supabase';
 
+const ARWEAVE_GATEWAY = process.env.ARWEAVE_GATEWAY;
+
 const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const slug = req.query.s as string;
-  const match = /^([^\/@]+)\/([^\/@]+)@([^\/]+)\/(.*)$/.exec(slug);
+  const match = /^(?<authorName>[^\/]+)\/(?<moduleName>[^\/]+)\/(?<versionName>[^\/]+)\/(?<filepath>.*)$/.exec(slug);
 
-  if (!match) {
+  if (!match?.groups) {
     res.status(404);
     res.end('Not Found');
   } else {
-    const [, authorName, moduleName, versionName, filepath] = match;
+    const { authorName, moduleName, versionName, filepath } = match.groups;
 
     if (!authorName) {
       res.status(404);
@@ -25,43 +27,67 @@ const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse
       res.status(404);
       res.end('Not Found');
     } else {
-      let { data: Files, error } = await supabase
-        .from('File')
-        .select('url, mimeType')
+      let { data: Versions, error } = await supabase
+        .from('Version')
+        .select('manifestid')
         .eq('authorName', authorName)
         .eq('moduleName', moduleName)
-        .eq('versionName', versionName)
-        .eq('path', filepath);
+        .eq('name', versionName);
 
       if (error) {
         res.status(500);
         res.end('Internal Server Error');
         throw new Error(`${error.message} (hint: ${error.hint})`);
-      } else if (Files.length < 1) {
+      } else if (Versions.length < 1) {
         res.status(404);
         res.end('Not Found');
-      } else if (Files.length > 1) {
-        // more than one files with same primary key @@id([author, module, version, path])
+      } else if (Versions.length > 1) {
+        // more than one versions with same primary key @@id([author, module, version, path])
         res.status(500);
         res.end('Internal Server Error');
         throw new Error(
-          `Found ${Files.length} "Files" with primary key @@id([${authorName}, ${moduleName}, ${versionName}, ${filepath}])`
+          `Found ${Versions.length} "Versions" with primary key @@id([${authorName}, ${moduleName}, ${versionName}, ${filepath}])`
         );
       } else {
-        const { url, mimeType } = Files[0];
+        let { data: Files, error } = await supabase
+          .from('File')
+          .select('*')
+          .eq('authorName', authorName)
+          .eq('moduleName', moduleName)
+          .eq('versionName', versionName)
+          .eq('path', filepath);
 
-        const data = await fetch(url);
+        if (error) {
+          res.status(500);
+          res.end('Internal Server Error');
+          throw new Error(`${error.message} (hint: ${error.hint})`);
+        } else if (Files.length < 1) {
+          res.status(404);
+          res.end('Not Found');
+        } else if (Files.length > 1) {
+          // more than one files with same primary key @@id([author, module, version, path])
+          res.status(500);
+          res.end('Internal Server Error');
+          throw new Error(
+            `Found ${Files.length} "Files" with primary key @@id([${authorName}, ${moduleName}, ${versionName}, ${filepath}])`
+          );
+        } else {
+          const { manifestid } = Versions[0];
+          const { mimeType } = Files[0];
 
-        res.setHeader('Content-Type', mimeType);
-        res.setHeader('Cache-Control', [
-          'public',
-          'maxage=31536000',
-          's-maxage=31536000',
-          'stale-while-revalidate=31536000',
-          'immutable',
-        ]);
-        res.status(data.status);
-        data.body.pipe(res);
+          const data = await fetch(`https://${ARWEAVE_GATEWAY}/${manifestid}/${filepath}`);
+
+          res.setHeader('Content-Type', mimeType);
+          res.setHeader('Cache-Control', [
+            'public',
+            'maxage=31536000',
+            's-maxage=31536000',
+            'stale-while-revalidate=31536000',
+            'immutable',
+          ]);
+          res.status(data.status);
+          data.body.pipe(res);
+        }
       }
     }
   }
