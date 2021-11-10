47ead40dbb89a2ae2ca4fc7077b8bf7874719799
Maximous Black <maximousblk@gmail.com>
2021-11-10 07:06:31 +0000

    fest: add homepage

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ module.exports = {
   async rewrites() {
     return [
       {
+        source: '/',
+        destination: '/deno-import-intellisense.json',
+      },
+      {
         source: '/.well-known/deno-import-intellisense.json',
         destination: '/deno-import-intellisense.json',
       },
